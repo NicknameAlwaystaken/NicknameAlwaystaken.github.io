a87930fda87e64e2ea5798caed39dfff3aca9441
NicknameAlwaystaken <boxx96@hotmail.com>
2023-03-16 09:50:12 +0200

    Testing markdown change on the fly

--- Showcase/js/mainFunctions.js
+++ Showcase/js/mainFunctions.js
@@ -1,9 +1,20 @@
+var counter = 0
+
 function homePage() {
-    window.location.href = "https://nicknamealwaystaken.github.io";
-  }
+  window.location.href = "https://nicknamealwaystaken.github.io";
+}
 function practicePage() {
-    window.location.href = "https://nicknamealwaystaken.github.io/PracticePage";
-  }
+  window.location.href = "https://nicknamealwaystaken.github.io/PracticePage";
+}
 function showcasePage() {
-    window.location.href = "https://nicknamealwaystaken.github.io/Showcase";
+  window.location.href = "https://nicknamealwaystaken.github.io/Showcase";
+}
+function changeMarkdown() {
+  if(counter % 2 == 0) {
+    document.getElementById("p1").src = "README.md";
   }
+  else {
+    document.getElementById("p1").src = "MARKDOWN.md";
+  }
+  counter++;
+}
