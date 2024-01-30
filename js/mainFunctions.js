d2fcdeeafb907fa977254ccd1fbe45ad2146cc64
NicknameAlwaystaken <NicknameAlwaystaken@users.noreply.github.com>
2024-01-30 13:31:25 +0200

    removing practice page

--- js/mainFunctions.js
+++ js/mainFunctions.js
@@ -2,9 +2,6 @@
 function homePage() {
   window.location.href = "https://nicknamealwaystaken.github.io";
 }
-function practicePage() {
-  window.location.href = "https://nicknamealwaystaken.github.io/PracticePage";
-}
 function showcasePage() {
   window.location.href = "https://nicknamealwaystaken.github.io/Showcase";
 }
